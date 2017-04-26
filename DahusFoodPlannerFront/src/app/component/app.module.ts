b192e4ed37c9c53dd2baea78797b666b5cc2eb88
Sandrine <sandrine.soudant@gmail.com>
2017-04-26 21:45:40 +0200

    Mise en place de la navigation.

--- DahusFoodPlannerFront/src/app/component/app.module.ts
+++ DahusFoodPlannerFront/src/app/component/app.module.ts
@@ -15,6 +15,7 @@ import { MealListComponent } from './meal-list/meal-list.component';
 import { DayEntryComponent } from './day-entry/day-entry.component';
 import { DayListComponent } from './day-list/day-list.component';
 
+import { RouterModule } from '@angular/router';
 import { RecipeService } from '../service/recipe.service';
 import { DayService } from 'app/service/day.service';
 import { ShoppingService } from 'app/service/shopping.service';
@@ -42,11 +43,26 @@ import { CategoryComponent } from './category/category.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'shopping',
+        component: ShoppingListComponent
+      },
+      {
+        path: 'days',
+        component: DayListComponent
+      },
+      {
+        path: '**',
+        component : ShoppingListComponent
+      }
+    ])
   ],
-  providers: [RecipeService, 
+  providers: [RecipeService,
     DayService,
     ShoppingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
