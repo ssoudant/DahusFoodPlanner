a7eefcd6c9ecaec76b46c13e207eebf9854b203e
Dahu <dahu88@gmail.com>
2017-10-31 23:49:00 +0100

    Ajoute plus de valeurs dans les données de tests.

--- db/src/main/resources/sql/init.sql
+++ db/src/main/resources/sql/init.sql
@@ -3,7 +3,10 @@ CREATE TABLE daylist (
 	title VARCHAR(32)
 );
 
-INSERT INTO daylist("id", "title") VALUES (1, 'Semaine 42');
+INSERT INTO daylist("id", "title")
+VALUES
+	(1, 'Semaine 42'),
+	(2, 'Semaine 43');
 
 CREATE TABLE day (
 	id BIGINT PRIMARY KEY,
@@ -12,4 +15,11 @@ CREATE TABLE day (
 	FOREIGN KEY (day_list_id) REFERENCES daylist
 ); 
 
-INSERT INTO day("id", "name", "day_list_id") VALUES (1, 'Samedi', 1);
+INSERT INTO day("id", "name", "day_list_id")
+VALUES
+	(1, 'Samedi', 1),
+	(2, 'Dimanche', 1),
+	(3, 'Lundi', 1),
+	(4, 'Samedi', 2),
+	(5, 'Dimanche', 2),
+	(6, 'Lundi', 2);
