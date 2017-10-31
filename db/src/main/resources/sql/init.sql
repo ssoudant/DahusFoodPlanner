3276825764ce6d17ff07ae13ac3b7f436bab1131
Dahu <dahu88@gmail.com>
2017-10-31 22:08:13 +0100

    Connecte l'API Rest à la base de données.

--- db/src/main/resources/sql/init.sql
+++ db/src/main/resources/sql/init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE daylist (
-	id INTEGER PRIMARY KEY,
+	id BIGINT PRIMARY KEY,
 	title VARCHAR(32)
 );
 
 INSERT INTO daylist("id", "title") VALUES (1, 'Semaine 42');
 
 CREATE TABLE day (
-	id INTEGER PRIMARY KEY,
+	id BIGINT PRIMARY KEY,
 	name VARCHAR(16),
-	daylist INTEGER NOT NULL,
-	FOREIGN KEY (daylist) REFERENCES daylist
+	day_list_id BIGINT NOT NULL,
+	FOREIGN KEY (day_list_id) REFERENCES daylist
 ); 
 
-INSERT INTO day("id", "name", "daylist") VALUES (1, 'Samedi', 1);
+INSERT INTO day("id", "name", "day_list_id") VALUES (1, 'Samedi', 1);
